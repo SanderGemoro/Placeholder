f2a958f9b79f95b63393385b6f9199898ebd7cd7
Sander van't Veer <sander@gemoro.nl>
2020-09-09 14:29:48 +0200

    Initial commit

---
+++ app/src/main/java/nl/nulldevelopment/placeholder/MainActivity.kt
@@ -0,0 +1,11 @@
+package nl.nulldevelopment.placeholder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
